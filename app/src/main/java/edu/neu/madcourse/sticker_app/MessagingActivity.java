9fb864ed2893011edaa8dfe5178fd5cd8b6017e7
crileyy <criley0007@gmail.com>
2021-03-22 16:04:11 -0400

    Handle orientation change on messaging screen

--- app/src/main/java/edu/neu/madcourse/sticker_app/MessagingActivity.java
+++ app/src/main/java/edu/neu/madcourse/sticker_app/MessagingActivity.java
@@ -22,6 +22,7 @@ import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -35,6 +36,8 @@ import edu.neu.madcourse.sticker_app.models.User;
 public class MessagingActivity extends AppCompatActivity {
 
     private final String SERVER_KEY = "key=AAAAYwQf6u0:APA91bFZ1hImC_ptJGPIScoJIUAsvUP2ORuOJkkFfCkWkWT3dGS6SnMtZ2u0U3hhpDkVwzOJLvdfXAH2KN6fqzSW4Zi4JgkK8n4wOIwMhpevlTCvVyGmzIxc0RRNab-89lK6m7MRbQgR";
+    private static final String KEY_OF_INSTANCE = "KEY_OF_INSTANCE";
+    private static final String NUMBER_OF_ITEMS = "NUMBER_OF_ITEMS";
     String CLIENT_KEY = "";
     String TAG = "MessagingActivity";
 
@@ -53,7 +56,7 @@ public class MessagingActivity extends AppCompatActivity {
 
         stickers = new ArrayList<>();
         stickers.add(new StickerCard("hi", "hello"));
-        createRecyclerView();
+        init(savedInstanceState);
 
         numStickersReceived = findViewById(R.id.num_stickers_received);
 
@@ -154,6 +157,52 @@ public class MessagingActivity extends AppCompatActivity {
     }
 
 
+    // Handling Orientation Changes on Android
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+
+        int size = stickers == null ? 0 : stickers.size();
+        outState.putInt(NUMBER_OF_ITEMS, size);
+
+        // Need to generate unique key for each item
+        // This is only a possible way to do, please find your own way to generate the key
+        for (int i = 0; i < size; i++) {
+            // put image information into instance
+            outState.putString(KEY_OF_INSTANCE + i + "0", stickers.get(i).image);
+            // put sender information into instance
+            outState.putString(KEY_OF_INSTANCE + i + "1", stickers.get(i).sender);
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+    private void init(Bundle savedInstanceState) {
+        initialItemData(savedInstanceState);
+        createRecyclerView();
+    }
+
+    private void initialItemData(Bundle savedInstanceState) {
+        // Not the first time to open this Activity
+        if (savedInstanceState != null && savedInstanceState.containsKey(NUMBER_OF_ITEMS)) {
+            if (stickers == null || stickers.size() == 0) {
+
+                int size = savedInstanceState.getInt(NUMBER_OF_ITEMS);
+
+                // Retrieve keys we stored in the instance
+                for (int i = 0; i < size; i++) {
+                    String image = savedInstanceState.getString(KEY_OF_INSTANCE + i + "0");
+                    String sender = savedInstanceState.getString(KEY_OF_INSTANCE + i + "1");
+
+                    StickerCard itemCard = new StickerCard(image, sender);
+
+                    stickers.add(itemCard);
+                }
+            }
+        }
+        // The first time to open this Activity
+        else {
+            // empty list for now
+        }
+    }
 
 
 }
