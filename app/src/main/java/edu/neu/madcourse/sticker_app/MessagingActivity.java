1dc5058b861c8cc9bf76c4174a49552c96bcbf0a
Tymofii Kryvtsun <kryvtsun.t@husky.neu.edu>
2021-03-22 22:27:15 +0200

    created a sendNotification method

--- app/src/main/java/edu/neu/madcourse/sticker_app/MessagingActivity.java
+++ app/src/main/java/edu/neu/madcourse/sticker_app/MessagingActivity.java
@@ -1,8 +1,11 @@
 package edu.neu.madcourse.sticker_app;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
@@ -26,6 +29,7 @@ import com.google.firebase.iid.InstanceIdResult;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -54,6 +58,8 @@ public class MessagingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        createNotificationChannel();
+
         setContentView(R.layout.activity_messaging);
 
         stickers = new ArrayList<>();
@@ -168,6 +174,18 @@ public class MessagingActivity extends AppCompatActivity {
             NotificationManager notificationManager = getSystemService(NotificationManager.class);
             notificationManager.createNotificationChannel(channel);
         }
+    }
+
+    public void sendNotification(View view){
+        Intent intent = new Intent(this, MessagingActivity.class);
+        PendingIntent newIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
+        String channelId = getString(R.string.channel_id);
+        Notification notification = new NotificationCompat.Builder(this,channelId)
+                .setContentTitle("Stick it")
+                .setContentText("New sticker received").setSmallIcon(R.drawable.earth).setContentIntent(newIntent).build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notification.flags |= Notification.FLAG_AUTO_CANCEL ;
+        notificationManager.notify(0, notification);
     }
 
 
